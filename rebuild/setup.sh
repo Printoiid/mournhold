6390d4862adfde59eed05cacafa29732f8f4c349
Printoiid <wdklebe@gmail.com>
2025-10-10 23:17:15 -0500

    Added kitty config

--- rebuild/setup.sh
+++ rebuild/setup.sh
@@ -2,16 +2,24 @@
 # We should always update apt repos
 sudo apt-get update
 
-# Get Steam and Nvidia drivers installed (The basics of course)
-yes | sudo apt-get install steam nvidia-driver-575 git tree spotify-client keepassxc npm wget python3-pip flameshot vlc vivaldi-stable nicotine
+# Get all the goodies
+xargs sudo apt-get -y install < aptpackages.txt
 
-# Make the desktop pretty
+echo "Making the desktop pretty!"
 gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark-Aqua"
 gsettings set org.cinnamon.theme name "Mint-Y-Dark-Aqua"
 gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y-Dark-Aqua"
 gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y-Aqua"
 gsettings set org.cinnamon panels-enabled "['1:0:top']"
 echo "Theme set and panel moved to the top!"
+
+echo "Setting up custom keymaps"
+custombindpath="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/"
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['${custombindpath}flameshot-screenshot/']"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/flameshot-screenshot/ binding "<Ctrl><Shift>3"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/flameshot-screenshot/ command "flameshot gui --accept-on-select --clipboard --path ~/Pictures/$(date '+%a-%M:%H-%h-%d-%Y')"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "flameshot-screenshot"
 
 # If I want brew installed + used packages
 # source ./brew.sh
