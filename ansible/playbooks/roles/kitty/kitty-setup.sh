b3fdd762e87fdbfe6cef2e1abea6101547f51863
Printoiid <wdklebe@gmail.com>
2026-01-19 21:55:23 -0600

    lots of changes. Dynamic kitty session for Ansible

--- ansible/playbooks/roles/kitty/kitty-setup.sh
+++ ansible/playbooks/roles/kitty/kitty-setup.sh
@@ -1,26 +1,33 @@
 echo "Starting - Kitty install"
+
 # Pull down the installer and run it
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # Create symbolic links to add kitty and kitten to PATH
 # (assuming ~/.local/bin is in your system-wide PATH)
 ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+
 # Link the kitty.desktop file somewhere it can be found by the OS
 ln -s ~/Mournhold/kitty/assets/kitty.desktop ~/.local/share/applications/
+
 # Link the entire kitty directory into .config for use
 ln -s ~/Mournhold/kitty/ ~/.config/
+
 # If you want to open text files and images in kitty via your
 # file manager also add the kitty-open.desktop file
 cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
 
 # Copy over sessions and settings
 string=$(cat << 'EOF'
+
 # Set up Kitty alias when launching via CLI
 alias kitty='kitty --override background_image_layout=cscaled --override background_image=$(ls $HOME/.config/kitty/img/*.png | sort --random-sort | head -1)'
+
 # Kitty sessions
 alias kubelab='kitty --session sessions/kubelab.kitty-session'
 EOF
 )
+
 # Add the aliases and sessions to .bashrc
 cat "$string" >> ~/.bashrc
 
---
+++ ansible/session-setup.sh
@@ -0,0 +1,85 @@
+write_tab () {
+	name=$1
+	filePath=$2
+	echo "new_tab $name" >> $filePath
+}
+
+layout_option() {
+	type=$1
+	filePath=$2
+	echo "layout $type" >> $filePath
+}
+
+launch_session () {
+	title=$1
+	command=$2
+	filePath=$3
+	echo "launch --title \"$title\" $command" >> $filePath
+}
+
+# Logging
+yellow='\033[1;33m'
+green='\033[0;32m'
+blue='\033[0;34m'
+cyan='\033[0;36m'
+nc='\033[0m'
+
+# Session Variables
+projectPath=$(pwd)
+sessionName=ansible
+sessionFile=$sessionName.kitty-session
+sessionFilePath="$projectPath/$sessionFile"
+
+# Delete the session file and remake it.
+if [ -f $sessionFile ]; then
+	rm $sessionFile && touch $sessionFile
+else
+	touch $sessionFile
+fi
+
+# Start customizing here
+# Get the project path and start building the session
+for dir in $projectPath/playbooks/roles/*; do
+	printf "${blue}Found: '${dir##*/}' directory under '${dir%/}'\n${nc}"
+	playbookRoleNames+=("${dir##*/}")
+	playbookRolePaths+=("${dir%/}")
+done
+
+# Get the inventory setup, and groupvars.
+printf "${cyan}Created tab: Inventory/GroupVars\n${nc}"
+write_tab "Inventory/GroupVars" $sessionFilePath
+layout_option "tall" $sessionFilePath
+launch_session "inventory" "nvim $projectPath/inventory/hosts.ini" $sessionFilePath
+for file in $projectPath/inventory/group_vars/*; do
+	printf "\t${green}Created session: $file\n${nc}"
+	launch_session "${file##*/}" "nvim $file" $sessionFilePath
+done
+
+# Main Playbook
+printf "${cyan}Created tab: Linux-Playbook\n${nc}"
+write_tab "Linux-Playbook" $sessionFilePath
+linuxPlaybook=$projectPath/playbooks/linux.yml
+launch_session ${linuxPlaybook##*/} "nvim $linuxPlaybook" $sessionFilePath
+
+# Playbooks
+printf "${cyan}Created tab: All-Playbooks\n${nc}"
+write_tab "All-Playbooks" $sessionFilePath
+layout_option "grid" $sessionFilePath
+for playbook in $projectPath/playbooks/*.yml; do
+	printf "\t${green}Created session: ${playbook##*/}\n${nc}"
+	launch_session ${playbook##*/} "nvim $playbook" $sessionFilePath
+done
+
+# Roles
+for role in ${playbookRolePaths[@]}; do
+	roleName=${role##*/}
+	printf "${cyan}Created tab: $roleName\n${nc}"
+	write_tab "Role-${role##*/}" $sessionFilePath
+	launch_session "All-Values" "nvim $projectPath/inventory/group_vars/all.yml" $sessionFilePath
+	for yml in $role/*/*; do
+		printf "\t${green}Created session: ${yml##*/}\n${nc}"
+		launch_session ${yml##*/} "nvim $yml" $sessionFilePath
+	done
+done
+
+cp $sessionFilePath ~/.config/kitty/sessions/$sessionFile
