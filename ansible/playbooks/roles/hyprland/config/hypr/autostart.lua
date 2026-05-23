9510fb1c3cdfa4fece58d562b9a2ad88dd1d39b9
Printoiid <wdklebe@gmail.com>
2026-05-22 23:08:55 -0500

    Updated auto start apps, forced environment to dark mode/theme

--- ansible/playbooks/roles/hyprland/config/hypr/autostart.lua
+++ ansible/playbooks/roles/hyprland/config/hypr/autostart.lua
@@ -11,12 +11,15 @@ require("programs")
 
 hl.on("hyprland.start", function()
 	-- Force Dark Theme
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'") -- for GTK3 apps
 	hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'") -- for GTK4 apps
-	hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-dark'") -- for GTK3 apps
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme 'Breeze Dark'") -- for GTK4 apps
+
 	hl.env("QT_QPA_PLATFORMTHEME", "qt6ct") -- for Qt apps# Theme
 	-- NOTE: Hyprland Items
 	hl.exec_cmd("hypridle & hyprsunset & waybar & hyprpaper")
 	hl.exec_cmd("nm-applet")
 	-- NOTE: Applications
-	hl.exec_cmd("spotify-launcher & pavucontrol & keepassxc")
+	hl.exec_cmd("spotify-launcher & pavucontrol")
+	hl.exec_cmd("xrandr --output DP-1 --primary")
 end)
